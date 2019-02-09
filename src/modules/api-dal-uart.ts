16fe26d8aa24e4840cdb38a72bfa984cec61d3b0
Anthony Vizcarra <Anthony@sphero.com>
2019-02-08 20:57:54 -0700

    fixed bugs in uart dal

--- src/modules/api-dal-uart.ts
+++ src/modules/api-dal-uart.ts
@@ -29,26 +29,72 @@ class ApiDalUart extends ApiDalBase {
         super();
 
         logger.debug('Starting');
-        return;
 
         this._apiParser = ApiParserFactory.getApiParser();
-        this._apiParser.apiMessageParsedCallback = this.onApiMessageParsed;
-        this._apiParser.apiProtocolErrorCallback = this.onApiProtocolError;
+
+        this._apiParser.apiMessageParsedCallback = (apiMessage: IApiMessage): void => {
+            logger.debug(`API Message parsed: ${apiMessage.prettyPrint()}`);
+
+            let mapKey: string = this.getApiMessageMapKey(apiMessage);
+            if (!this._apiCommandPendingResponseMap.has(mapKey)) {
+                logger.error(`API Message key not found: ${mapKey}`);
+                // TODO: do what?
+            }
+
+            let responsePromise: DeferredPromise<IApiResponseMessage> | undefined = this._apiCommandPendingResponseMap.get(mapKey);
+            if (responsePromise) {
+                logger.debug('Promise resolved.');
+
+                this._apiCommandPendingResponseMap.delete(mapKey);
+                responsePromise.resolve(apiMessage);
+
+                return;
+            }
+
+            logger.warning(`Key size: ${this._apiCommandPendingResponseMap.size}`);
+            logger.warning('NO promise found!');
+
+            // TODO: do what?
+
+            // TODO: what about async messages to the socket?
+        };
+
+        this._apiParser.apiProtocolErrorCallback = (errorCode: number): void => {
+            logger.error(`API Protocol Error: '${ApiProtocolErrorCodes.getApiProtocolErrorMessageFromCode(errorCode)}' (${errorCode})`);
+        };
 
         this._serialPort = new SerialPort(path, {
             autoOpen: false,
             baudRate: baudRate
         });
 
-        this._serialPort.on('open', this.onOpen);
-        this._serialPort.on('close', this.onClose);
-        this._serialPort.on('data', this.onDataReceived);
-        this._serialPort.on('drain', this.onDrain);
-        this._serialPort.on('error', this.onError);
+        this._serialPort.on('open', (): void => {
+            logger.debug('Serial Port opened');
+        });
+        this._serialPort.on('close', (): void => {
+            logger.debug('Serial Port closed');
+        });
+        this._serialPort.on('data', (data: Array<number>): void => {
+            // var dataUTF8 = data.toString('utf-8');
+
+            logger.debug(`Received bytes: ${ByteConversionUtilities.convertNumbersToHexCsvString(data)}`);
+
+            this._apiParser.queueBytes(data);
+        });
+        this._serialPort.on('drain', (): void => {
+            logger.debug('Serial Port drained');
+        });
+        this._serialPort.on('error', (error: Error): void => {
+            logger.error(`An error occurred on Serial Port: '${error}'`);
+        });
 
         this._apiCommandPendingResponseMap = new Map<string, DeferredPromise<IApiResponseMessage>>();
 
         this._serialPort.open(error => {
+            if (!error) {
+                return;
+            }
+
             logger.error(`An error occurred while opening Serial Port: '${error}'`);
         });
 
@@ -61,53 +107,14 @@ class ApiDalUart extends ApiDalBase {
         // });
     }
 
-    private onApiMessageParsed(apiMessage: IApiMessage): void {
-        logger.debug(`API Message parsed: ${apiMessage.prettyPrint()}`);
-
-        let mapKey: string = this.getApiMessageMapKey(apiMessage);
-        if (!this._apiCommandPendingResponseMap.has(mapKey)) {
-            // TODO: do what?
-        }
-
-        let responsePromise: DeferredPromise<IApiResponseMessage> | undefined = this._apiCommandPendingResponseMap.get(mapKey);
-        if (responsePromise) {
-            this._apiCommandPendingResponseMap.delete(mapKey);
-            responsePromise.resolve(apiMessage);
-        }
-
-        // TODO: do what?
-
-        // TODO: what about async messages to the socket?
-    }
-    private onApiProtocolError(errorCode: number): void {
-        logger.error(`API Protocol Error: '${ApiProtocolErrorCodes.getApiProtocolErrorMessageFromCode(errorCode)}' (${errorCode})`);
-    }
-
-    private onOpen(): void {
-        logger.debug('Serial Port opened');
-    }
-    private onClose(): void {
-        logger.debug('Serial Port closed');
-    }
-    private onDataReceived(data: Array<number>): void {
-        // var dataUTF8 = data.toString('utf-8');
-
-        logger.debug(`Received bytes: ${ByteConversionUtilities.convertNumbersToHexCsvString(data)}`);
-
-        this._apiParser.queueBytes(data);
-    }
-    private onDrain(): void {
-        logger.debug('Serial Port drained');
-    }
-    private onError(error: Error): void {
-        logger.error(`An error occurred on Serial Port: '${error}'`);
-    }
-
     protected async sendApiCommandMessageInternal(apiCommandMessage: IApiCommandMessage): Promise<IApiResponseMessage> {
+        logger.debug(`Attempting to send API Command Message: ${apiCommandMessage.prettyPrint()}`);
+
         let responsePromise: DeferredPromise<IApiResponseMessage> = new DeferredPromise<IApiResponseMessage>();
         let mapKey: string = this.getApiMessageMapKey(apiCommandMessage);
 
         this._apiCommandPendingResponseMap.set(mapKey, responsePromise);
+        logger.warning(`Key size: ${this._apiCommandPendingResponseMap.size}`);
 
         // TODO: do we need buffer the bytes in case we need to drain?
 
@@ -121,7 +128,12 @@ class ApiDalUart extends ApiDalBase {
     }
 
     private getApiMessageMapKey(apiMessage: IApiMessage): string {
-        return `${apiMessage.sequenceNumber}.${apiMessage.targetId}.${apiMessage.sourceId}.${apiMessage.deviceId}.${apiMessage.commandId}`
+        let mapKey: string = `${apiMessage.sequenceNumber}.${apiMessage.deviceId}.${apiMessage.commandId}`;
+        //${apiMessage.targetId}.${apiMessage.sourceId}.
+
+        logger.debug(`Generated API Message key: '${mapKey}'`);
+
+        return mapKey;
     }
 }
 
