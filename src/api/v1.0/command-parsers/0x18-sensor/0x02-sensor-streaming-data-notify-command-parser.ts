28d71f944acb0fe43de8f18fe76eea34b9fd8ce9
Clara Richter <clara.richter@sphero.com>
2019-08-12 11:23:54 -0600

    now sending data in a simple array

--- src/api/v1.0/command-parsers/0x18-sensor/0x02-sensor-streaming-data-notify-command-parser.ts
+++ src/api/v1.0/command-parsers/0x18-sensor/0x02-sensor-streaming-data-notify-command-parser.ts
@@ -2,35 +2,18 @@
 import {ByteConversionUtilities} from '../../../../utilities/byte-conversion-utilities'
 
 
-export function parseSensorStreamingDataNotifyResponse(dataRawBytes: Array<number>): IGetStreamingData {
+export function parseSensorStreamingDataNotifyResponse(dataRawBytes: Array<number>): Array<number> {
+
+    let bytesPerData: number = 4;
     let currentIndex: number = 0;
 
-    // Index: 0 | Name: 'pitch' | Type: 'float' | Size: 1
-    let pitchBytes: Array<number> = ByteConversionUtilities.getFloatBytes(dataRawBytes, currentIndex);
-    let pitch: number = ByteConversionUtilities.byteArrayToFloat(pitchBytes.reverse());
-    currentIndex += pitchBytes.length;
+    let parsedData : Array<number> = [];
 
-    // Index: 1 | Name: 'roll' | Type: 'float' | Size: 1
-    let rollBytes: Array<number> = ByteConversionUtilities.getFloatBytes(dataRawBytes, currentIndex);
-    let roll: number = ByteConversionUtilities.byteArrayToFloat(rollBytes.reverse());
-    currentIndex += rollBytes.length;
-
-    // Index: 2 | Name: 'yaw' | Type: 'float' | Size: 1
-    let yawBytes: Array<number> = ByteConversionUtilities.getFloatBytes(dataRawBytes, currentIndex);
-    let yaw: number = ByteConversionUtilities.byteArrayToFloat(yawBytes.reverse());
-    currentIndex += yawBytes.length;
-
-    let getStreamingData: IGetStreamingData = {
-        pitch: pitch,
-        roll: roll,
-        yaw: yaw
-    };
-
-    return getStreamingData;
+    for(let currentIndex = 0; currentIndex < dataRawBytes.length; currentIndex += bytesPerData){
+        let currentIndexBytes: Array<number> = ByteConversionUtilities.getFloatBytes(dataRawBytes, currentIndex);
+        let data: number = ByteConversionUtilities.byteArrayToFloat(currentIndexBytes.reverse());
+        parsedData.push(data);
+    }
+    return parsedData;
 }
 
-export interface IGetStreamingData {
-    readonly pitch: number;
-    readonly roll: number;
-    readonly yaw: number;
-}
